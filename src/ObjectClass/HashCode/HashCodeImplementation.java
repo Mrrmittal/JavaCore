765357516541d86ef0e7c88ae1265994dab492e3
Jatin Mittal <jatinmittal164@gmail.com>
2024-01-14 01:38:16 +0530

    equals() method Practice

---
+++ src/ObjectClass/HashCode/HashCodeImplementation.java
@@ -0,0 +1,45 @@
+package src.ObjectClass.HashCode;
+
+public class HashCodeImplementation{
+    public static void main(String[] args) {
+
+    Employee e1 = new Employee("Manoj", "CTO");
+    Employee e2 = new Employee("Manoj", "CTO");
+
+    /*
+        System.out.println(e1.hashCode());
+        System.out.println(e2.hashCode());
+
+        If we do not define the hashcode method below hashcode output will be the different for the upr both similar
+        value Object. Because we can say every property with different diff hashCode.
+
+        System.out.println(e1.name.hashCode());
+        System.out.println(e2.name.hashCode());
+
+        If we check hashcode upper specific field it will be the same.
+     */
+
+        System.out.println(e1.hashCode());
+        System.out.println(e2.hashCode()); //If we do not define the
+
+    }
+}
+
+
+class Employee{
+    String name;
+    String designation;
+
+    public Employee(String name,String designation){
+        this.name = name;
+        this.designation = designation;
+    }
+
+//    @Override
+//    public int hashCode(){
+//        return (name + designation).hashCode();
+//    }
+
+}
+
+
