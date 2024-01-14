86ee1944255bbe5de328647ec9be84dbbae31b0c
Jatin Mittal <jatinmittal164@gmail.com>
2024-01-15 01:41:29 +0530

    Collection Framework Practice

---
+++ src/CollectionFramwork/CollectionFrameworkDefinition.java
@@ -0,0 +1,19 @@
+package src.CollectionFramwork;
+
+public class CollectionFrameworkDefinition {
+
+    /*
+Que: What is Collection Framework and use of Collection Framework in Java?
+
+Solution : As we Know Java is an Object-Oriented Programing language. So we can not imagine object-Orientation
+           programming without using Data Structure and Algorithms. So, Basically Collections Frameworks help us to
+           provide the data store property and then Some basic Implementation on it.
+
+           Like : Add Object into the collection.
+                  Remove element from the collection.
+                  Replace element from the collection.
+                  sorting data in ascending and vice verse
+                  Iteration.
+     */
+
+}
---
+++ src/CollectionFramwork/CollectionVsCollections.java
@@ -0,0 +1,18 @@
+package src.CollectionFramwork;
+
+public class CollectionVsCollections {
+    /*
+    1. Collection (Interface) :- Collection is an Interface in java. That help to data storage and implementation on it.
+                                 Basically we can say that its provide the data structure taste in java.
+                                 With included HashSet, Arraylist, LinkedList ect.
+
+    2. Collections (Class)    :- Collections is a class in Java that have some in Built method help to manipulation the
+                                 data of HashSet, Arraylist, LinkedList.
+
+                                 Like : Collections.sort();
+                                        Collections.replace();
+                                        Collections.copy();
+                                        Collections.shuffle();
+     */
+
+}
