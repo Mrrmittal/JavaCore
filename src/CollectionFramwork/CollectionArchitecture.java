8ec04955477c5b3fbe3e6e8af16b39ed9bb1a1fa
Jatin Mittal <jatinmittal164@gmail.com>
2024-01-15 01:17:37 +0530

    Collection Framework Practice

---
+++ src/CollectionFramwork/CollectionArchitecture.java
@@ -0,0 +1,26 @@
+package src.CollectionFramwork;
+
+
+
+
+
+
+public class CollectionArchitecture {
+
+/*
+                            Iterable
+
+                            Collection Interface
+
+          List                                 Queue                                       Set
+      1. ArrayList(Class)                   1. Deque(Interface)                    1. HashSet(Class) Implement Set
+      2. Linked List(Class)                 2. PriorityQueue(Class)                2. LinkedHashSet(Class) Implement Set
+      3. Vector(Class)                      3. ArrayDeque(Class)                   3. SortedSet(Interface) Extends Set
+      4. Stack(Class)                                                              4. TreeSet(Class) Implement SortedSet
+
+
+ */
+
+
+
+}
