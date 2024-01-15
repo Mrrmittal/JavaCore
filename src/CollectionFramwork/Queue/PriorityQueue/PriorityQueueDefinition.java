947a9991dc6c69de3ddc286ec8b38a0a25f89123
Jatin Mittal <jatinmittal164@gmail.com>
2024-01-16 03:06:38 +0530

    Collection Practice

---
+++ src/CollectionFramwork/Queue/PriorityQueue/PriorityQueueDefinition.java
@@ -0,0 +1,22 @@
+package src.CollectionFramwork.Queue.PriorityQueue;
+
+public class PriorityQueueDefinition {
+    /*
+    Que: What is PriorityQueue? Basic Operations on PriorityQueue and properties?
+    Sol: PriorityQueue is a class in java that's implements Queue Interface in Java. But as we know Queue Interface
+         only follow FIFO (First In First Out). But sometimes we need to process data as per their priority and this
+         priority all are set by the heap memory. So, while we need to represent data as per the priority we follow
+         PriorityQueue class collection framework.
+
+      #Properties:-
+      1. Duplicate allowed.
+      2. Null value does not allowed.
+      3. Priority base data arrangement.
+      4. Does not allow mixture of data element.
+
+      #Syntax:
+
+      PriorityQueue ab = new PriorityQueue();
+
+     */
+}
