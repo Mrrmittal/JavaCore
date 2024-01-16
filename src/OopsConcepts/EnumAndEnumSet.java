7cf7200a6d2e0c379b7ebd10b9e50f437eda71ed
Jatin Mittal <jatinmittal164@gmail.com>
2024-01-16 15:20:36 +0530

    Enum Practice

---
+++ src/OopsConcepts/EnumAndEnumSet.java
@@ -0,0 +1,27 @@
+package src.OopsConcepts;
+
+import java.util.EnumSet;
+
+public class EnumAndEnumSet {
+    /*
+    Enum : Enum is special class of java that store some constant variables that are unchanged and file.
+    EnumSet : Special class that help to represent the variables of Enum class collection.
+                Methods Included: allOf(), clone()
+              There are not special constructor exists in EnumSet
+     */
+//    Declaration of an Enum
+    public static void main(String[] args) {
+
+
+        enum status {
+            Active, Running, Paused
+        }
+
+//    How to get all variable of enum
+
+        EnumSet enumSet = EnumSet.allOf(status.class);
+        System.out.println(enumSet);
+
+
+    }
+}
