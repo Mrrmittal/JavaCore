77f1de7baf5c380faa6783e7c73eb7a4800c06f4
Jatin Mittal <jatinmittal164@gmail.com>
2024-01-29 19:21:51 +0530

    Threads Practice.

---
+++ src/Threads/BasicConcepts/SynchronizationTypes.java
@@ -0,0 +1,13 @@
+package src.Threads.BasicConcepts;
+
+public class SynchronizationTypes {
+/*
+Types of Synchronization ?
+                                                Synchronization
+ 1. Process Based Synchronization: Does not use in Java Multi-thread Programming.
+ 2. Threads Synchronization :
+        a) Mutual Exclusive: can be achieved by 3 ways : Synchronization method, Synchronization block and
+                                                         static Synchronization
+        b) Cooperation : Can be achieved by 3 ways : by Object class method wait(), notifyAll(), notify()
+ */
+}
